ab0778e8bbec91804324574762bf615fbbf9ee20
ANSH CHAUHAN <146896630+Anshchauhanhub@users.noreply.github.com>
2026-04-01 19:04:10 +0530

    Update logo image source path in TopNav component

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -5,7 +5,7 @@ const TopNav = () => {
   return (
     <nav className="fixed top-0 w-full z-50 bg-[#131315]/80 backdrop-blur-xl flex justify-between items-center px-6 h-16 w-full">
       <div className="flex items-center lg:hidden">
-        <img src="/logo.png" alt="Axiom Logo" className="w-48 h-auto object-contain drop-shadow-[0_0_20px_rgba(253,184,19,0.5)]" />
+        <img src="frontend/public/logo.png" alt="Axiom Logo" className="w-48 h-auto object-contain drop-shadow-[0_0_20px_rgba(253,184,19,0.5)]" />
       </div>
       <div className="flex items-center gap-6">
         <div className="hidden md:flex gap-8 items-center">
